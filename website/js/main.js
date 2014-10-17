803ed8597b661d8f2c5e3986fb1a4387d7ca4ab2
= <hack@metahack.com>
2014-10-17 10:36:24 -0700

    javascript alert now works on button click

--- website/js/main.js
+++ website/js/main.js
@@ -1,8 +1,16 @@
 console.log("Message sent.");
 
+function onload() {
+   var button = document.getElementById("label01");
+   // add onclick event 
+   button.onclick = function() { 
+        alert("alert");
+   }
+}
+
 function talkingCat() {
 
 	alert("ACK!");
 }
 
-Document.getElementbyId("title").
+/* Document.getElementbyId("title"). */
